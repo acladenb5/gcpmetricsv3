a1a8c5033725262c7533b0ec636eec8cd5a8bf8d
A. Claden <acladen@purplespark.org>
2018-08-20 12:18:17 -0400

    Add metrics list as yaml and json

--- gcplbn/gcplbn.py
+++ gcplbn/gcplbn.py
@@ -1,7 +1,21 @@
 """Main application."""
 # import os
 import sys
+import argparse
 
+# pylint: disable-msg=line-too-long
+# pylint: disable-msg=too-many-arguments
+# pylint: disable-msg=too-many-locals
+# pylint: disable-msg=broad-except
+# pylint: disable-msg=too-many-branches
+# pylint: disable-msg=protected-access
+# pylint: disable-msg=too-many-statements
+
+
+PARSER = argparse.ArgumentParser(
+    description="Get metrics from GCP",
+    formatter_class=argparse.RawDescriptionHelpFormatter
+)
 
 def main():
     """Main routine."""
