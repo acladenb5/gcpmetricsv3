8e571870d55d559dbf64a6ff707377419dc22dd2
A. Claden <acladen@purplespark.org>
2018-08-21 10:52:54 -0400

    Add keyfile management

--- gcplbn/gcplbn.py
+++ gcplbn/gcplbn.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import argparse
+import json
 import yaml
 # from google.cloud import monitoring_v3
 # from google.oauth2 import service_account
@@ -14,6 +15,7 @@ import yaml
 # pylint: disable-msg=protected-access
 # pylint: disable-msg=too-many-statements
 # pylint: disable-msg=duplicate-code
+# pylint: disable-msg=invalid-name
 
 
 PARSER = argparse.ArgumentParser(
@@ -25,6 +27,9 @@ PARSER.add_argument("--version", default=None, action='store_true', help='Print 
 PARSER.add_argument('--keyfile', help='Goolge Cloud Platform service account key file.', metavar='FILE')
 PARSER.add_argument('--project', help='Project ID.', metavar='ID')
 PARSER.add_argument('--service', help='Cloud service to check', metavar='SVC')
+PARSER.add_argument('--privatekeyid', help='Private key ID', metavar='PKID')
+PARSER.add_argument('--privatekey', help='Private key content', metavar='PKC')
+PARSER.add_argument('--clientid', help='Client ID', metavar='CID')
 
 
 def error(message):
@@ -57,7 +62,39 @@ def main():
     if not args_dict['project']:
         error('--project not specified')
 
-    # project_id = args_dict['project']
+    if not args_dict['privatekeyid']:
+        error('--privatekeyid not specified')
+
+    if not args_dict['privatekey']:
+        error('--privatekey not specified')
+
+    if not args_dict['clientid']:
+        error('--clientid not specified')
+
+    project_id = args_dict['project']
+    private_key_id = args_dict['privatekeyid']
+    private_key = args_dict['privatekey']
+    private_key = private_key.replace('\\n', '\n')
+    client_email = 'lbn-monitoring@' + project_id + '.iam.gserviceaccount.com'
+    client_x509_cert_url = 'https://www.googleapis.com/robot/v1/metadata/x509/lbn-monitoring%40' + project_id + '.iam.gserviceaccount.com'
+    client_id = args_dict['clientid']
+    keyfile_name = 'keyfiles/' + project_id + '.json'
+
+    KEYFILE = {'type': 'service_account',
+               'project_id': project_id,
+               'private_key_id': private_key_id,
+               'private_key': private_key,
+               'client_email': client_email,
+               'client_id': client_id,
+               'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+               'token_uri': 'https://oauth2.googleapis.com/token',
+               'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+               'client_x509_cert_url': client_x509_cert_url}
+
+    print('keyfile:\n{}'.format(KEYFILE))
+
+    with open(keyfile_name, 'w') as fp:
+        json.dump(KEYFILE, fp)
 
     # if not args_dict['keyfile']:
     #     client = monitoring_v3.MetricServiceClient()
