b356a5ea69c49ccf9246ac4ebee9848a3c34f535
A. Claden <acladen@purplespark.org>
2018-08-23 14:36:38 -0400

    Stuct correct

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	cmdline "github.com/acladenb5/gocmdline"
+	yaml "gopkg.in/yaml.v2"
 )
 
+// Variables
 var (
 	// Version is the software version, set at compile time
 	Version string
@@ -14,11 +18,100 @@ var (
 	Build string
 )
 
+// Structs
+
+// Services structure
+// type Services struct {
+// 	Services []struct {
+// 		Service string `yaml:"service"`
+// 		Metrics []struct {
+// 			Metric []string
+// 		}
+// 	}
+// }
+type Services struct {
+	Services map[string][]string
+}
+
+// Metric structure
+type Metric struct {
+	Metric string
+}
+
+// MetricsList structure
+type MetricsList struct {
+	MetricsList []Metric // `yaml:"metrics"`
+}
+
+// Service structure
+type Service struct {
+	Service string   `yaml:"service"`
+	Metrics []Metric `yaml:"metrics"`
+	// Metrics MetricsList `yaml:"metrics"`
+}
+
+// ServicesList structure
+type ServicesList struct {
+	ServicesList []Service `yaml:"services"`
+	// Services []string `yaml:"services"`
+}
+
+// Main routine
 func main() {
 	cmdline := cmdline.New()
+	cmdline.AddFlag("v", "version", "Display version and exits")
 	cmdline.AddFlag("d", "debug", "Enable debug messages")
-	cmdline.AddOption("p", "project", "project_id", "Project to get metrics from")
+	cmdline.AddOption("m", "metrics", "metrics", "File containing the list of available metrics")
+	cmdline.AddOption("p", "project", "project", "Project to get metrics from")
+	cmdline.AddOption("s", "service", "service", "Service to check")
 	cmdline.Parse(os.Args)
 
-	fmt.Println(os.Args[0] + " " + Version + " - " + Build[0:9])
+	if cmdline.IsOptionSet("v") {
+		// fmt.Println(os.Args[0] + " - Version " + " - Build " + Build[0:9])
+		log.Println(os.Args[0] + " - Version ")
+		os.Exit(0)
+	}
+
+	if !cmdline.IsOptionSet("project") {
+		log.Fatalln("Project is not set")
+	}
+
+	if !cmdline.IsOptionSet("service") {
+		log.Fatalln("Service is not set")
+	}
+
+	var serviceListFile string
+	var projectID = cmdline.OptionValue("project")
+	var serviceID = cmdline.OptionValue("service")
+
+	if !cmdline.IsOptionSet("metrics") {
+		serviceListFile = "./metrics_list.yaml"
+	} else {
+		serviceListFile = cmdline.OptionValue("metrics")
+	}
+
+	fmt.Println(projectID, serviceID, serviceListFile)
+
+	// var svcList ServicesList
+	var mySvc Services
+
+	svList, errSvcList := ioutil.ReadFile(serviceListFile)
+
+	if errSvcList != nil {
+		log.Fatalln(errSvcList)
+	}
+
+	// fmt.Println(source)
+	// fmt.Println(reflect.TypeOf(svList))
+	// fmt.Println(reflect.TypeOf(svcList))
+
+	// erryaml := yaml.Unmarshal(svList, &svcList)
+	erryaml := yaml.Unmarshal(svList, &mySvc)
+
+	if erryaml != nil {
+		log.Fatalln(erryaml)
+	}
+
+	// fmt.Printf("%v\n", svcList)
+	fmt.Printf("%v\n", mySvc.Services[serviceID])
 }
