bfd489d51a09987b68bf773b6c2c05e82a5f556e
A. Claden <acladen@purplespark.org>
2018-09-20 15:06:02 -0400

    Remove an unused variable

--- gcpmonitoring.sh
+++ gcpmonitoring.sh
@@ -5,4 +5,5 @@ GCP_METRICS_DIR="/PATH/TO/REPOSITORY_CLONE"
 
 # ----- DO NOT EDIT BELOW THIS LINE ----- #
 source ${VENV_DIR}/bin/activate
-python ${GCP_METRICS_DIR}/GCP-Monitoring/gcplbn/gcplbn.py $*
+cd ${GCP_METRICS_DIR}/GCP-Monitoring
+python gcplbn/gcplbn.py $*
